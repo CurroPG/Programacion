67fbc5f4000b36008dc54c1d78a12b9df6aaa690
Curro Portillo <211curroportillo@gmail.com>
2025-10-15 19:40:41 +0200

    ejericicio 7 y 8

--- java/tema 4/ejercicio_04_08/App.java
+++ java/tema 4/ejercicio_04_08/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) {
+
+        System.out.print("Primera nota: ");
+        double nota1 = Double.parseDouble(System.console().readLine());
         
+        System.out.print("Segunda nota: ");
+        double nota2 = Double.parseDouble(System.console().readLine());
+        
+        System.out.print("Tercera nota: ");
+        double nota3 = Double.parseDouble(System.console().readLine());
+
+        double notaMedia = (nota1 + nota2 + nota3)/3;
+
+        String boletin = "";
+        switch(Math.round((int)notaMedia)){
+            case 1, 2, 3, 4 -> boletin = "Insuficiente";
+            case 5 -> boletin = "Suficiente";
+            case 6 -> boletin = "Bien";
+            case 7, 8 -> boletin = "Notable";
+            case 9, 10 -> boletin = "Sobresaliente";
+            default -> System.out.println("Error.");
+        }
+        
+        System.out.println("La media es " + notaMedia);
+        System.out.println(boletin);
     }
 }
