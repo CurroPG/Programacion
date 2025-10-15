03ee5e29f8be797f79dc9f5969716328177ffa84
Curro Portillo <211curroportillo@gmail.com>
2025-10-15 18:45:57 +0200

    ejercicio 28 intento1

---
+++ java/tema 4/ejercicio_04_28/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) {
+        
+        System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijera.");
+        String turno1 = System.console().readLine();
+
+        System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijera.");
+        String turno2 = System.console().readLine();
+
+
+        if (turno1 == turno2)
+            System.out.println("Empate");
+        
+
+        switch (turno1){
+            case "piedra" ->{
+                if (turno2.equals("papel"))
+                    System.out.println("Gana el jugaador 2");
+                    else
+                        System.out.println("Gana el jugaador 1");
+            }
+            case "papel" ->{
+                if (turno2.equals("tijera"))
+                    System.out.println("Gana el jugador 2");
+                    else
+                        System.out.println("Gana el jugaador 1");
+            }
+            case "tijera" ->{
+                if (turno2.equals("piedra"))
+                    System.out.println("Gana el jugador 2");
+                    else
+                        System.out.println("Gana el jugaador 1");
+            }
+            default ->{
+                System.out.println("Introduzca piedra, papel o tijera.");
+            }
+        }
+
+    }
+}
